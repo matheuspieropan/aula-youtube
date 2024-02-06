006884f285d1ab193c469c51ecddbf6d71637f3c
mamapisouza <Pi204326@>
2024-02-06 10:37:32 -0300

    bean

---
+++ Spring Framework/annotation/3- Bean/src/main/java/com/example/bean/BeanApplication.java
@@ -0,0 +1,12 @@
+package com.example.bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanApplication.class, args);
+	}
+}
---
+++ Spring Framework/annotation/3- Bean/src/test/java/com/example/bean/BeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
