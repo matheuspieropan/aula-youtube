65eacc7743856f00226ce943db56c8c38a41fcd5
mamapisouza <Pi204326@>
2024-03-07 23:48:40 -0300

    Criação inicial projeto demostrativo para criar imagem docker

---
+++ docker/src/main/java/com/pieropan/docker/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.pieropan.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String imprimir() {
+        return "Hello";
+    }
+}
