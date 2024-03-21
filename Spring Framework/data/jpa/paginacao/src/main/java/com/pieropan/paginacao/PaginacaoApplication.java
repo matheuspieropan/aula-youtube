e3891e935c99bf586e2527763a2bef28d7cbea2b
mamapisouza <Pi204326@>
2024-03-21 18:41:29 -0300

    Implementação da paginação com JPA

---
+++ Spring Framework/data/jpa/paginacao/src/main/java/com/pieropan/paginacao/PaginacaoApplication.java
@@ -0,0 +1,12 @@
+package com.pieropan.paginacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginacaoApplication.class, args);
+    }
+}
