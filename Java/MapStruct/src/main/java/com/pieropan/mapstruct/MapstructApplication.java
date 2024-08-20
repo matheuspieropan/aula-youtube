9f2a8efe993e9fad0dccdacd85acd386c9045793
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2024-08-20 09:55:58 -0300

    Remoção imports

--- Java/MapStruct/src/main/java/com/pieropan/mapstruct/MapstructApplication.java
+++ Java/MapStruct/src/main/java/com/pieropan/mapstruct/MapstructApplication.java
@@ -10,8 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class MapstructApplication {
