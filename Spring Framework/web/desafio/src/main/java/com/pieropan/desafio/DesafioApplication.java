8661ecd19a9c4205db13a78a83da270c13cc4cec
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2025-03-23 13:20:52 -0300

    desafio implementação

---
+++ Spring Framework/web/desafio/src/main/java/com/pieropan/desafio/DesafioApplication.java
@@ -0,0 +1,12 @@
+package com.pieropan.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioApplication.class, args);
+    }
+}
---
+++ Spring Framework/web/desafio/src/test/java/com/pieropan/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pieropan.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
